97502fbed2b4415cb9bdaad403f9b217245244f5
Prometehus <ikhwan7teha@gmail.com>
2025-08-19 17:13:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -3,5 +3,13 @@ const withNextIntl = require('next-intl/plugin');
 const withIntl = withNextIntl('./src/i18n.ts');
  
 module.exports = withIntl({
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
   // Other Next.js configuration ...
 });
