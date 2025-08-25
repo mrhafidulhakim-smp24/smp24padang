adde311aa89be52b8e4ff16773ce02b08d04ed45
Prometehus <ikhwan7teha@gmail.com>
2025-08-25 11:41:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,22 @@
-
+import { Pool } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
-
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
-//
-// Learn more: https://pris.ly/d/help/next-js-best-practices
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+let prisma: PrismaClient;
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV === 'production') {
+  const neon = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaNeon(neon);
+  prisma = new PrismaClient({ adapter });
+} else {
+  if (!globalForPrisma.prisma) {
+    const neon = new Pool({ connectionString: process.env.DATABASE_URL });
+    const adapter = new PrismaNeon(neon);
+    globalForPrisma.prisma = new PrismaClient({ adapter });
+  }
+  prisma = globalForPrisma.prisma;
+}
 
 export default prisma;
