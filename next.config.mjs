a1e9e8925012903b3a16f4dc1dd723e4f177fca7
IngsR <ikhwan2ende@gmail.com>
2025-09-01 17:16:06 +0700

    finishing cms

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,3 @@
-import createNextIntlPlugin from 'next-intl/plugin';
- 
-const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
- 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -26,4 +22,4 @@ const nextConfig = {
   },
 };
  
-export default withNextIntl(nextConfig);
+export default nextConfig;
