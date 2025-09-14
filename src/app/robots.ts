238b38142a7f55a816dd21dd18337c495132fd51
IngsR <ikhwan2ende@gmail.com>
2025-09-14 10:18:55 +0700

    adjust & optimization robot.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,14 +3,16 @@ import { MetadataRoute } from 'next';
 // This will use the NEXT_PUBLIC_SITE_URL environment variable.
 // Make sure to set it in your Vercel project settings.
 // Fallback is for the current Vercel deployment URL.
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://smpn24padang.vercel.app';
+const siteUrl =
+    process.env.NEXT_PUBLIC_SITE_URL || 'https://smpn24padang.sch.id';
 
 export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
-    sitemap: `${siteUrl}/sitemap.xml`,
-  };
+    return {
+        rules: {
+            userAgent: '*',
+            allow: '/',
+            disallow: ['/admin/', '/api/', '/login/'],
+        },
+        sitemap: `${siteUrl}/sitemap.xml`,
+    };
 }
