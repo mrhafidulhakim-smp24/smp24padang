9b9001343dc2b077f7686269a59f0b970c917563
IngsR <ikhwan2ende@gmail.com>
2025-10-11 19:42:19 +0700

    adjust header

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         ignoreDuringBuilds: true,
     },
     images: {
-        // unoptimized: true,
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
